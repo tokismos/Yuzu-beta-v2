74b61ef562a3dc1c2d2b54ecff45a1888b77ad6b
edjubert <edouard.jubert@gmail.com>
2022-05-15 23:57:54 +0200

    chore: deleted unessessary files

--- node_modules/react-native-head-tab-view/HeaderContext.tsx
+++
@@ -1,4 +0,0 @@
-
-import React from 'react';
-import { IHeaderContext } from './types'
-export const HeaderContext = React.createContext<IHeaderContext | undefined>(undefined);
--- node_modules/react-native-head-tab-view/index.js
+++
@@ -1,9 +0,0 @@
-import { ScrollView, FlatList, SectionList } from 'react-native'
-import createCollapsibleScrollView from './createCollapsibleScrollView'
-export { HeaderContext } from './HeaderContext'
-export { default as GestureContainer } from './GestureContainer'
-
-const HScrollView = createCollapsibleScrollView(ScrollView)
-const HFlatList = createCollapsibleScrollView(FlatList)
-const HSectionList = createCollapsibleScrollView(SectionList)
-export { HScrollView, HFlatList, HSectionList }
